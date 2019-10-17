935711bc163be7c18e3ef1776ea3340994b09f76
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-17 12:08:50 -0600

    Add post functionality

--- src/App/App.js
+++ src/App/App.js
@@ -19,12 +19,23 @@ class App extends Component {
           reservations: data
         })
       })
+      .catch(error => console.log(error))
   }
 
   addRes = newRes => {
-    this.setState({
-      reservations: [...this.state.reservations, newRes]
-    })
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(newRes),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    fetch('http://localhost:3001/api/v1/reservations', options)
+      .then(res => res.json())
+      .then(data => this.setState({
+        reservations: [...this.state.reservations, newRes]
+      }))
+      .catch(error => console.log(error))
   }
 
   render() {
