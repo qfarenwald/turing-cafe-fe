8ebf8d4d5cf8b4aadd325355687f3cf60f8da91b
Quinne Farenwald <q.farenwald@gmail.com>
2019-10-17 09:45:53 -0600

    Remove key as prop

--- src/Res/Res.js
+++ src/Res/Res.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Res.css';
 
-const Res = ({ key, id, name, date, time, number }) => {
+const Res = ({ id, name, date, time, number }) => {
   return (
     <section>
       <h3>{name}</h3>
